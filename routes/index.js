f0c3b9fa8512922a75c96e6f8237ee39f0f5901f
Lakshmi Sowjanya Bommu <lakshmib@ispace.com>
2020-01-25 12:22:28 +0530

    gitmyslot

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//  res.render('index', { title: 'Express' });
+var model = {
+ title: 'Azure',
+ message: process.env.MESSAGE || "This is developnment"
+};
+res.render('index',model);
 });
 
 module.exports = router;
